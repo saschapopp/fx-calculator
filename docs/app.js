e863f14115463b56d0dd2f069f875382d596669c
= <=>
2019-05-04 11:23:24 +0200

    init

---
+++ docs/app.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('select').forEach(element => {
+    element.innerHTML =`
+    <option value="EUR">EUR</option>
+    <option value="USD">USD</option>
+    <option value="CHF">CHF</option>
+    <option value="SEK">SEK</option>`;
+});
