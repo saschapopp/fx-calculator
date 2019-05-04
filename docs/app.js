4ec5ac73f38b44f5184d445c42546e529747695b
= <=>
2019-05-04 13:13:44 +0200

    Upload

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,20 @@
+const fixerUri = 'https://saschapopp.github.io/fx-calculator/rates.json'
+
 document.querySelectorAll('select').forEach(element => {
     element.innerHTML =`
     <option value="EUR">EUR</option>
     <option value="USD">USD</option>
     <option value="CHF">CHF</option>
-    <option value="SEK">SEK</option>`;
+    <option value="SEK">SEK</option>
+    `;
 });
+
+function convert(inputValue, inputCurrency, outputCurrency) {
+    fetch(fixerUri)
+    .then(response => {
+        if (response.status == 200) {return response.json();
+        } else {
+            return Promise.reject('failed to download rates');
+        }
+    }).then((rates) => {})
+}
